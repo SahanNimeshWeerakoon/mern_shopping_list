907eb01725d239a908ab4bc04dc87cc06114b435
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2024-12-18 15:12:57 +0530

    connect to dtabase
    - insert data to database on request

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-
+const pool = require('./db');
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1)",
+            [ description ]
+        );
+        return res.json(newTodo)
+    } catch(err) {
+        console.log(err)
+    }
+});
 
 app.listen(5000, () => {
     console.log("listening to port 5000");
